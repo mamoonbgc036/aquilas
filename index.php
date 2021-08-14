29c75ccd8ad73c4bc8296d1779f6f5291a889a48
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-08-14 23:54:52 +0600

    14.08.2021

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php get_header(); ?>
-    <div class="content">
-        content
+    <div id="primary">
+        <main id="main">
+            <?php
+                if ( have_posts() ){
+                    while ( have_posts() ) : the_post();
+                        the_title();
+                        the_content();
+                endwhile;
+                }
+            ?>
+        </main>
     </div>
 <?php get_footer(); ?>
  
